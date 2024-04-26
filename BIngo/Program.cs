c913bbbc8f3cd851b5e41d2dba2a25bba7cfa1a1
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-26 09:04:38 -0300

    cartelas funcionando

--- BIngo/Program.cs
+++ BIngo/Program.cs
@@ -1,48 +1,69 @@
-﻿int[] numeros_sorteados = new int[98];
-int[,] cartela = new int[5, 5];
-int indice = 0, linha = 0, coluna = 0;
-
-while (indice < numeros_sorteados.Length)
+﻿int[] popular_matrizes = new int[98];
+int[,] cartela1 = new int[5, 5];
+int indice = 0;
+for (int i = 0; i < popular_matrizes.Length; i++)
 {
-    int sorteio = new Random().Next(1, 100);
-    bool numero_repetido = false;
-    for (int i = 0; i < indice; i++)
-    {
-        if (sorteio == numeros_sorteados[i])
-        {
-            numero_repetido = true;
-            break;
-        }
-    } if (!numero_repetido)
-    {
-        numeros_sorteados[indice] = sorteio;
-        indice++;
-    }
+    popular_matrizes[i] = i;
 }
-for (int i = 0; i < numeros_sorteados.Length; i++)
-    Console.Write(numeros_sorteados[i] + " ");
-
-
-for (int i = 0; i < 5; i++)
+int Gerar_Vetor_Aleatorios(int tamanho_vetor) 
+{
+    int[] numeros_sorteados = new int[tamanho_vetor];
+    while (indice < tamanho_vetor)
     {
-    for (int j = 0; j < 5; j++)
-    {
-       int contador = new Random().Next(1, 100);
-        if (numeros_sorteados[contador] > 0)
+        int sorteio = new Random().Next(1, 100);
+        bool numero_repetido = false;
+        for (int i = 0; i < indice; i++)
         {
-            cartela[i, j] = numeros_sorteados[contador];
-            numeros_sorteados[contador ] = 0;
-            
+            if (sorteio == numeros_sorteados[i])
+            {
+                numero_repetido = true;
+                break;
+            }
+        }
+        if (!numero_repetido)
+        {
+            numeros_sorteados[indice] = sorteio;
+            indice++;
         }
     }
+    for (int i = 0; i < numeros_sorteados.Length; i++)
+        Console.Write(numeros_sorteados[i] + " ");
+    return numeros_sorteados[tamanho_vetor];
+}
+int[,] Gerar_Cartelas()
+{
+    int linha = 0, coluna = 0;
+    int[,] cartela = new int[5,5];
     
+    while (linha < 5)
+    {
+        while (true)
+        {
+            int j = new Random().Next(1, 5);
+            if (popular_matrizes[j] != 0)
+            {
+                cartela[linha, coluna] = popular_matrizes[j];
+                popular_matrizes[j] = 0;
+                coluna++;
+                if (coluna > 4)
+                {
+                    linha++;
+                    coluna = 0;
+                }
+                break;
+            }
+        }
+    }
+
+    return cartela; 
 }
-Console.WriteLine();
+
+cartela1 = Gerar_Cartelas();
 for (int i = 0;i < 5; i++)
 {
     for(int j = 0; j < 5; j++)
     {
-        Console.Write(cartela[i,j] + " ");
+        Console.Write(cartela1[i,j] + " ");
     }
     Console.WriteLine();
 }
